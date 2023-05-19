d6739e54558fca0c55b5d05dc6a0ac310edbecbb
IvanZeng <Ivan_zq@foxmail.com>
2023-05-19 20:30:56 +0000

    Validation using Joi

--- src/config/dependencies.js
+++ src/config/dependencies.js
@@ -1,18 +1,24 @@
-import AccountsRepositoryInMemory from '../accounts/repositories/InMemoryRepository';
-import AccountsRepositoryMongo from '../accounts/repositories/MongoAccountRepository';
+import AccountsRepositoryInMemory from "../accounts/repositories/InMemoryRepository";
+import AccountsRepositoryMongo from "../accounts/repositories/MongoAccountRepository";
+import AccountSchema from '../accounts/validators';
 
 const buildDependencies = () => {
-  const dependencies = {
-  };
-  if (process.env.DATABASE_DIALECT === "in-memory") {
-    dependencies.accountsRepository = new AccountsRepositoryInMemory();
-  } else if (process.env.DATABASE_DIALECT === "mongo") {
-    dependencies.accountsRepository = new AccountsRepositoryMongo();
-  } else if (process.env.DATABASE_DIALECT === "mysql") {
-    throw new Error('Add MySQL support');
-  } else {
-    throw new Error('Add DB Support to project');
-  }
-  return dependencies;
+
+    const dependencies = {
+        accountSchema: AccountSchema
+    };
+
+    if (process.env.DATABASE_DIALECT === "in-memory") {
+        dependencies.accountsRepository = new AccountsRepositoryInMemory();
+    } else if (process.env.DATABASE_DIALECT === "mongo") {
+        dependencies.accountsRepository = new AccountsRepositoryMongo();
+    } else if (process.env.DATABASE_DIALECT === "mysql") {
+        throw new Error('Add MySQL support');
+    } else {
+        throw new Error('Add DB Support to project');
+    }
+
+    return dependencies;
 };
+
 export default buildDependencies;
