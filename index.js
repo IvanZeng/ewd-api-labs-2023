600cdb790547a1a65d6a8734a1898fb97f158aee
IvanZeng <Ivan_zq@foxmail.com>
2023-05-19 19:55:44 +0000

    Connected to MangoDB

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import genresRouter from './src/genres';
-import buildDependencies from "./src/config/dependencies";
 import createAccountsRouter from './src/accounts/routes';
+import buildDependencies from './src/config/dependencies';
 import createMoviesRouter from './src/movies/routes';
+import db from './src/config/db';
 
 dotenv.config();
+db.init();
 
-const dependencies = buildDependencies();
 const app = express();
-const port = process.env.PORT;
 
+const port = process.env.PORT;
+//.. Add this after db.init();
+const dependencies = buildDependencies();
 app.use(express.json());
 app.use('/api/movies', createMoviesRouter(dependencies));
 app.use('/api/genres', genresRouter);
 app.use('/api/accounts', createAccountsRouter(dependencies));
-
 app.listen(port, () => {
-    console.info(`Server running at ${port}`);
+  console.info(`Server running at ${port}`);
 });
--- src/config/dependencies.js
+++ src/config/dependencies.js
@@ -1,19 +1,18 @@
-import AccountsRepositoryInMemory from "../accounts/repositories/InMemoryRepository";
+import AccountsRepositoryInMemory from '../accounts/repositories/InMemoryRepository';
+import AccountsRepositoryMongo from '../accounts/repositories/MongoAccountRepository';
 
 const buildDependencies = () => {
-    const dependencies = {
-    };
-
-    if (process.env.DATABASE_DIALECT === "in-memory") {
-        dependencies.accountsRepository = new AccountsRepositoryInMemory();
-    } else if (process.env.DATABASE_DIALECT === "mongo") {
-        throw new Error('Add Mongo Support');
-    } else if (process.env.DATABASE_DIALECT === "mysql") {
-        throw new Error('Add MySQL support');
-    } else {
-        throw new Error('Add DB Support to project');
-    }
-    return dependencies;
+  const dependencies = {
+  };
+  if (process.env.DATABASE_DIALECT === "in-memory") {
+    dependencies.accountsRepository = new AccountsRepositoryInMemory();
+  } else if (process.env.DATABASE_DIALECT === "mongo") {
+    dependencies.accountsRepository = new AccountsRepositoryMongo();
+  } else if (process.env.DATABASE_DIALECT === "mysql") {
+    throw new Error('Add MySQL support');
+  } else {
+    throw new Error('Add DB Support to project');
+  }
+  return dependencies;
 };
-
 export default buildDependencies;
