019d777b86d4954ad95caccaf48f763c350c59c2
IvanZeng <Ivan_zq@foxmail.com>
2023-05-19 21:26:01 +0000

    Finished Exercises for Genres Lab3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import genresRouter from './src/genres';
 import createAccountsRouter from './src/accounts/routes';
 import buildDependencies from './src/config/dependencies';
 import createMoviesRouter from './src/movies/routes';
+import createGenresRouter from './src/genres/routes';
 import db from './src/config/db';
 import errorHandler from './src/utils/ErrorHandler';
 
@@ -16,9 +16,9 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
-app.use('/api/movies', createMoviesRouter(dependencies));
-app.use('/api/genres', genresRouter);
 app.use('/api/accounts', createAccountsRouter(dependencies));
+app.use('/api/movies', createMoviesRouter(dependencies));
+app.use('/api/genres', createGenresRouter(dependencies));
 app.use(errorHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
